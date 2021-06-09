4728c4a02e4b06bff36fa534b2a8f2de10b2fea1
Ashmeet Kaur <ashmeet.kaur@comprotechnologies.com>
2021-06-09 20:19:07 +0530

    Minor chnages

--- src/cat.js
+++ src/cat.js
@@ -1,13 +1,21 @@
 export default class Cat {
   constructor(context) {
     this._name = 'Cat';
-    this._context = context
+    this._context = context;
   }
   get name() {
-    console.log("getname");
+    console.log('getname');
+
+    let contextElement = this._context.nativeElement;
+
+    let dummyDiv = contextElement.createElement('div');
+
+    dummyDiv.innerHTML = 'This is class page';
+    contextElement.appendChild(dummyDiv);
+
     return {
       name: this._name,
       context: this._context
-    }
+    };
   }
 }
